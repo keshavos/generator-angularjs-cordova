56755f24c97beed4abcc5f6482c7d1f11253b7a3
Keshav OS <keshav.os@gmail.com>
2014-04-30 10:05:21 +0100

    Fixes the correct base path for the grunt connect task

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function (grunt) {
                 options: {
                     open: true,
                     base: [
-                        '.tmp',
+                        'www/',
                         ''
                     ]
                 }
