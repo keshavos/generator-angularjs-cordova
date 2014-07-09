afb8a578028c566f627f63202e01f2f3e238e4a2
Keshav OS <keshav.os@gmail.com>
2014-07-09 11:24:24 +0100

    Moves all route files to template directory. Fixes references within the route sub-generator

--- angular-route/templates/_route.js
+++
@@ -1,6 +0,0 @@
-$stateProvider.
-		state('<%= slugifiedName %>', {
-			url: '/<%= slugifiedRoutePath %>',
-			templateUrl: 'app/modules/<%= slugifiedModuleName %>/views/<%= slugifiedViewName %>.html',
-            controller: '<%= classifiedControllerName %>Controller'
-		}).
---
+++ templates/javascript/_route.js
@@ -0,0 +1,6 @@
+$stateProvider.
+state('<%= slugifiedName %>', {
+    url: '/<%= slugifiedRoutePath %>',
+    templateUrl: 'app/modules/<%= slugifiedModuleName %>/views/<%= slugifiedViewName %>.html',
+    controller: '<%= classifiedControllerName %>Controller'
+}).
