337dd54a0899e60636773e567fcd96a6b81187a8
Keshav OS <keshav.os@gmail.com>
2014-07-09 11:32:06 +0100

    Moves all service files to template directory. Fixes references within the service sub-generator

--- angular-service/index.js
+++ angular-service/index.js
@@ -1,34 +1,34 @@
 'use strict';
 
 var util = require('util'),
-	yeoman = require('yeoman-generator');
+    yeoman = require('yeoman-generator');
 
 
 var ServiceGenerator = yeoman.generators.NamedBase.extend({
-	askForModuleName: function() {
-		var done = this.async();
+    askForModuleName: function() {
+        var done = this.async();
 
-		var prompts = [{
-			name: 'moduleName',
-			message: 'Which module does this service belongs to?',
-			default: 'core'
-		}];
+        var prompts = [{
+            name: 'moduleName',
+            message: 'Which module does this service belongs to?',
+            default: 'core'
+        }];
 
-		this.prompt(prompts, function(props) {
-			this.moduleName = props.moduleName;
-			this.slugifiedModuleName = this._.slugify(this._.humanize(this.moduleName));
-			
-			this.slugifiedName = this._.slugify(this.name);
-			this.classifiedName = this._.classify(this.slugifiedName);
-			this.humanizedName = this._.humanize(this.slugifiedName);
+        this.prompt(prompts, function(props) {
+            this.moduleName = props.moduleName;
+            this.slugifiedModuleName = this._.slugify(this._.humanize(this.moduleName));
 
-			done();
-		}.bind(this));
-	},
+            this.slugifiedName = this._.slugify(this.name);
+            this.classifiedName = this._.classify(this.slugifiedName);
+            this.humanizedName = this._.humanize(this.slugifiedName);
 
-	renderServiceFile: function() {
-		this.template('_service.js', 'www/app/modules/' + this.slugifiedModuleName + '/services/' + this.slugifiedName + '.js')
-	}
+            done();
+        }.bind(this));
+    },
+
+    renderServiceFile: function() {
+        this.template('../../templates/javascript/service/_service.js', 'app/modules/' + this.slugifiedModuleName + '/services/' + this.slugifiedName + '.js')
+    }
 });
 
 module.exports = ServiceGenerator;
