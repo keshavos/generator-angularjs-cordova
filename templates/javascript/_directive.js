4112297bffbed1b1c5783d3a80902078de369dcf
Keshav OS <keshav.os@gmail.com>
2014-07-09 10:57:35 +0100

    Moves all directive files to template directory. Fixes references within the directive sub-generator

---
+++ templates/javascript/_directive.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('<%= slugifiedModuleName %>').directive('<%= camelizedName %>', [
+
+    function() {
+        return {
+            // name: '',
+            // priority: 1,
+            // terminal: true,
+            // scope: {}, // {} = isolate, true = child, false/undefined = no change
+            // controller: function($scope, $element, $attrs, $transclude) {},
+            // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+            // restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+            // template: '',
+            // templateUrl: '',
+            // replace: true,
+            // transclude: true,
+            // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+            link: function($scope, iElm, iAttrs, controller) {
+
+            }
+        };
+    }
+]);
