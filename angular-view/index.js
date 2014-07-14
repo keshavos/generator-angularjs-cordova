a96b0c0c55b3c70d7b67a7cfeff3afbda2efd449
Keshav OS <keshav.os@gmail.com>
2014-07-14 09:54:03 +0100

    Fix angular route module to the new app path

--- angular-view/index.js
+++ angular-view/index.js
@@ -1,112 +1,112 @@
 'use strict';
 var util = require('util'),
-	fs = require('fs'),
-	yeoman = require('yeoman-generator');
+    fs = require('fs'),
+    yeoman = require('yeoman-generator');
 
 
 var ViewGenerator = yeoman.generators.NamedBase.extend({
-	askForModuleName: function() {
-		var done = this.async();
+    askForModuleName: function() {
+        var done = this.async();
 
-		var prompts = [{
-			name: 'moduleName',
-			message: 'Which module does this view belongs to?',
-			default: 'core'
-		}];
+        var prompts = [{
+            name: 'moduleName',
+            message: 'Which module does this view belongs to?',
+            default: 'core'
+        }];
 
-		this.prompt(prompts, function(props) {
-			this.moduleName = props.moduleName;
-			this.controllerName = props.controllerName;
+        this.prompt(prompts, function(props) {
+            this.moduleName = props.moduleName;
+            this.controllerName = props.controllerName;
 
-			this.slugifiedModuleName = this._.slugify(this.moduleName);
-			this.humanizedModuleName = this._.humanize(this.moduleName);
+            this.slugifiedModuleName = this._.slugify(this.moduleName);
+            this.humanizedModuleName = this._.humanize(this.moduleName);
 
-			this.slugifiedName = this._.slugify(this._.humanize(this.name));
-			this.classifiedName = this._.classify(this.slugifiedName);
-			this.humanizedName = this._.humanize(this.slugifiedName);
+            this.slugifiedName = this._.slugify(this._.humanize(this.name));
+            this.classifiedName = this._.classify(this.slugifiedName);
+            this.humanizedName = this._.humanize(this.slugifiedName);
 
-			done();
-		}.bind(this));
-	},
+            done();
+        }.bind(this));
+    },
 
-	askForControllerName: function() {
-		var done = this.async();
+    askForControllerName: function() {
+        var done = this.async();
 
-		var prompts = [{
-			name: 'controllerName',
-			message: 'What is the name of the controller this view will use?',
-			default: this.classifiedName
-		}];
+        var prompts = [{
+            name: 'controllerName',
+            message: 'What is the name of the controller this view will use?',
+            default: this.classifiedName
+        }];
 
-		this.prompt(prompts, function(props) {
-			this.controllerName = props.controllerName;
+        this.prompt(prompts, function(props) {
+            this.controllerName = props.controllerName;
 
-			this.slugifiedControllerName = this._.slugify(this.controllerName);
-			this.classifiedControllerName = this._.classify(this.slugifiedControllerName);
+            this.slugifiedControllerName = this._.slugify(this.controllerName);
+            this.classifiedControllerName = this._.classify(this.slugifiedControllerName);
 
-			done();
-		}.bind(this));
-	},
+            done();
+        }.bind(this));
+    },
 
-	askToAddRoute: function() {
-		var done = this.async();
+    askToAddRoute: function() {
+        var done = this.async();
 
-		var prompts = [{
-			type: 'confirm',
-			name: 'addRoute',
-			message: 'Would you like to add a route for this view?',
-			default: true
-		}];
+        var prompts = [{
+            type: 'confirm',
+            name: 'addRoute',
+            message: 'Would you like to add a route for this view?',
+            default: true
+        }];
 
-		this.prompt(prompts, function(props) {
-			this.addRoute = props.addRoute;
+        this.prompt(prompts, function(props) {
+            this.addRoute = props.addRoute;
 
-			done();
-		}.bind(this));
-	},
+            done();
+        }.bind(this));
+    },
 
-	askForRouteDetails: function() {
-		if (this.addRoute) {
-			var done = this.async();
+    askForRouteDetails: function() {
+        if (this.addRoute) {
+            var done = this.async();
 
-			var prompts = [{
-				name: 'routePath',
-				message: 'What is your view route path?',
-				default: this.slugifiedName
-			}];
+            var prompts = [{
+                name: 'routePath',
+                message: 'What is your view route path?',
+                default: this.slugifiedName
+            }];
 
-			this.prompt(prompts, function(props) {
-				this.routePath = props.routePath;
-				this.slugifiedRoutePath = this._.slugify(this.routePath);
+            this.prompt(prompts, function(props) {
+                this.routePath = props.routePath;
+                this.slugifiedRoutePath = this._.slugify(this.routePath);
 
-				done();
-			}.bind(this));
-		}
-	},
+                done();
+            }.bind(this));
+        }
+    },
 
-	renderRoute: function() {
-		if (this.addRoute) {
-			var routesFilePath = process.cwd() + '/www/app/modules/' + this.slugifiedModuleName + '/config/routes.js';
+    renderRoute: function() {
+        if (this.addRoute) {
+            var routesFilePath = process.cwd() + '/app/modules/' + this.slugifiedModuleName + '/config/routes.js';
 
             // If routes file exists we add a new state otherwise we render a new one
-			if (fs.existsSync(routesFilePath)) {
-				// Read the source routes file content
-				var routesFileContent = this.readFileAsString(routesFilePath);
+            if (fs.existsSync(routesFilePath)) {
+                // Read the source routes file content
+                var routesFileContent = this.readFileAsString(routesFilePath);
 
-				// Append the new state
-				routesFileContent = routesFileContent.replace('$stateProvider.', this.engine(this.read('_route.js'), this));
+                // Append the new state
+                routesFileContent = routesFileContent.replace('$stateProvider.', this.engine(this.read('_route.js'), this));
 
                 // Save route file
-				this.writeFileFromString(routesFileContent, routesFilePath);
-			} else {
-				this.template('_routes.js', 'app/modules/' + this.slugifiedModuleName + '/config/routes.js')
-			}
-		}
-	},
+                this.writeFileFromString(routesFileContent, routesFilePath);
+            } else {
+                this.template('_routes.js', 'app/modules/' + this.slugifiedModuleName + '/config/routes.js')
+            }
+        }
+    },
 
-	renderViewFile: function() {
-		this.template('_view.html', 'www/app/modules/' + this.slugifiedModuleName + '/views/' + this.slugifiedName + '.html')
-	}
+    renderViewFile: function() {
+        this.template('_view.html', 'www/app/modules/' + this.slugifiedModuleName + '/views/' + this.slugifiedName + '.html')
+    }
 });
 
 module.exports = ViewGenerator;
